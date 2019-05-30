36c5a6f48d90c100f22abe8f848884f32dd5bdec
moshedeitsch <41856538+moshedeitsch@users.noreply.github.com>
2019-05-30 14:51:32 +0300

    first Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Css/index.css'
+import Game from './Component/Game';
+
+ReactDOM.render(<Game />, document.getElementById('root'));
